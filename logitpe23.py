9ebdfcb7993374327835b6eab598809ae673c8da
zsh <sanderlapimaa@gmail.com>
2023-12-04 18:36:05 +0200

    edited some shi (im stupid)

--- logitpe23.py
+++ logitpe23.py
@@ -1,2 +1,2 @@
 pluh = input("what is your name? ") # this asks for your name
-print = ("your name is"),(pluh) # this will print your answer
+print("your name is"),(pluh) # this will print your answer
