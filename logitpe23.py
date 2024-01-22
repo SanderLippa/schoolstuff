ba9acaf5417a67dca898b374de4883a7f563b9a5
zsh <sanderlapimaa@gmail.com>
2024-01-22 13:56:44 +0200

    help 2

--- logitpe23.py
+++ logitpe23.py
@@ -2,5 +2,5 @@ pee=int(input("sisestage tuule kiirus "))
 if pee < 8:
     print("Kehalise kasvatuse tund võib toimuda õues")
 
-if peee > 8:
+if pee > 8:
     print("Kehalise kasvatuse tund peaks toimuma siseruumis")
