1d6b26b529d19dcaf86f2e406c4ba277a370c74e
zsh <sanderlapimaa@gmail.com>
2024-01-22 15:41:26 +0200

    fix

--- logitpe23.py
+++ logitpe23.py
@@ -1,10 +1,10 @@
 vanus=int(input("sisestage vanus "))
 if  16>vanus>=0:
-    print("Ei ole veel lubatud haaletada")
+    print("Ei ole veel lubatud h‰‰letada")
 elif 16<=vanus<18:
-    print("On lubatud haaletada kohalikel valimistel")
+    print("On lubatud h‰‰letada kohalikel valimistel")
 elif 18<=vanus<=110:
-    print("On lubatud haaletada nii kohalikel kui ka riigikogu valimistel")
+    print("On lubatud h‰‰letada nii kohalikel kui ka riigikogu valimistel")
 if vanus<0:
     print("Vigane vanus")
 elif vanus>110:
