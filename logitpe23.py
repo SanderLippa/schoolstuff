1f6ace42a92c3b43f597dd89a700774055737bd8
zsh <sanderlapimaa@gmail.com>
2024-01-29 14:38:55 +0200

    sex

--- logitpe23.py
+++ logitpe23.py
@@ -1,11 +1,12 @@
-vanus=int(input("sisestage vanus "))
-if  16>vanus>=0:
-    print("Ei ole veel lubatud hääletada")
-elif 16<=vanus<18:
-    print("On lubatud hääletada kohalikel valimistel")
-elif 18<=vanus<=110:
-    print("On lubatud hääletada nii kohalikel kui ka riigikogu valimistel")
-if vanus<0:
-    print("Vigane vanus")
-elif vanus>110:
-    print("Vigane vanus")
+emap = int(input("sisestage ema pikkus "))
+isap = int(input("sisestaga isa pikkus "))
+sugu = str(input("sisesta sugu "))
+if sugu == ("n"):
+    lapse_pikkus = (emap+isap) / 2 - 6.5
+elif sugu == ("N"):
+    lapse_pikkus = (emap+isap) / 2 - 6.5
+if sugu == ("m"):
+    lapse_pikkus = (emap+isap) / 2 + 6.5
+elif sugu == ("M"):
+    lapse_pikkus = (emap+isap) / 2 - 6.5
+print(lapse_pikkus)
