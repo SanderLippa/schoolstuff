0db81e72ff1ca630e23720794cf43d8248a5bf9b
zsh <sanderlapimaa@gmail.com>
2024-01-30 15:09:03 +0200

    1.5

--- logitpe23.py
+++ logitpe23.py
@@ -1,12 +1,21 @@
-emap = int(input("sisestage ema pikkus "))
-isap = int(input("sisestaga isa pikkus "))
-sugu = str(input("sisesta sugu "))
-if sugu == ("n"):
-    lapse_pikkus = (emap+isap) / 2 - 6.5
-elif sugu == ("N"):
-    lapse_pikkus = (emap+isap) / 2 - 6.5
-if sugu == ("m"):
-    lapse_pikkus = (emap+isap) / 2 + 6.5
-elif sugu == ("M"):
-    lapse_pikkus = (emap+isap) / 2 + 6.5
-print(lapse_pikkus)
+# algus
+aeg = float(input("sisestage aeg "))
+distants = float(input("sisestage distants "))
+
+#jooksu valemi kraam
+A_h = 0.14354
+B_h = 220
+C_h = 1.4
+
+#teise ajsa kraam
+A_js = 25.4347
+B_js = 18
+C_js = 1.81
+
+# ma soovin surra 
+ajaline = int(A_js*(B_js-aeg)**C_js)
+distantsiline = int(A_h*(distants-B_h)**C_h)
+
+#prindi tulemused
+print(ajaline)
+print(distantsiline)
