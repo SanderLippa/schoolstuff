7c74245edb22ff81eb8087775e163a7b9cc61ddc
zsh <sanderlapimaa@gmail.com>
2024-01-30 17:41:28 +0200

    fixed

--- logitpe23.py
+++ logitpe23.py
@@ -1,3 +1,9 @@
-alus = int(input("sisestage number "))
-astendaja = int(input("sisestage astendaja "))
-print(alus**astendaja)
+nimi = input("sisestage nimi ")
+lkiirus = int(input("lubatud kiirus "))
+tegelkiirus = int(input("tegelik kiirus "))
+trahv = tegelkiirus-lkiirus
+trahv2 = trahv*5
+if trahv2 <=300:
+    print(nimi,", kiiruse ületamise eest on teie trahv",trahv2, "eurot")
+else:
+    print(nimi,", kiiruse ületamise eest on teie trahv 300 eurot")
