f5467bcc52ceeb17021ce5288fda61afb82cf54c
zsh <sanderlapimaa@gmail.com>
2024-02-06 16:42:26 +0200

    3.1

--- logitpe23.py
+++ logitpe23.py
@@ -1,20 +1,7 @@
-arv1 = int(input("sisestage esimene arv "))
-tehe = (input("(+ |  -  | * | /) "))
-arv2 = int(input("sisestage teine arv "))
-
-if tehe == '/' and arv1 or arv2 ==0:
-    vastus = "Nulliga ei saa jagada."
-
-elif tehe == '+':
-    vastus = arv1+arv2
-    
-elif tehe == '-':
-    vastus = arv1-arv2
-    
-elif tehe == '*':
-    vastus = arv1*arv2
-    
-elif tehe == '/':
-    vastus = arv1/arv2
-
-print(vastus)
+vs = input("sisestage vanasona ")
+i = int(input("mitu korda korrata"))
+while i >0:
+    print(vs)
+    i -= 1
+    if i == 0:
+        break
