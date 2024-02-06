189f32c1dc2ccdce5ecfcab47e84a505b4f815e2
zsh <sanderlapimaa@gmail.com>
2024-02-06 16:20:53 +0200

    yipeeee

--- logitpe23.py
+++ logitpe23.py
@@ -1,11 +1,17 @@
-temp = int(input("sisestage temperatuur "))
-sun = input("kas päike paistab ")
-lipp = input("kas rannas on roheline lipp")
+arv1 = int(input("sisestage esimene arv "))
+tehe = (input("(+ |  -  | * | /) "))
+arv2 = int(input("sisestage teine arv "))
 
-if lipp == 'jah':
-    print("Võid minna randa!")
-else:
-    if temp >=20 and sun == 'jah':
-        print("Võid minna randa!")
-    elif temp <=19 or sun == 'ei':
-        print("Täna ei tasu randa minna.")
+if tehe == '+':
+    vastus = arv1+arv2
+    
+elif tehe == '-':
+    vastus = arv1-arv2
+    
+elif tehe == '*':
+    vastus = arv1*arv2
+    
+elif tehe == '/':
+    vastus = arv1/arv2
+
+print(vastus)
