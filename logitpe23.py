74efaf8265fe29636181338d3f08b71ecc98827a
zsh <sanderlapimaa@gmail.com>
2024-02-06 16:23:57 +0200

    cooking

--- logitpe23.py
+++ logitpe23.py
@@ -2,7 +2,10 @@ arv1 = int(input("sisestage esimene arv "))
 tehe = (input("(+ |  -  | * | /) "))
 arv2 = int(input("sisestage teine arv "))
 
-if tehe == '+':
+if tehe == '/' and arv1 or arv2 ==0:
+    vastus = "Nulliga ei saa jagada."
+
+elif tehe == '+':
     vastus = arv1+arv2
     
 elif tehe == '-':
