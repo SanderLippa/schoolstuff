377d00a75e53a600e51d2805ccf2a1ba34874c31
zsh <sanderlapimaa@gmail.com>
2024-02-19 13:06:31 +0200

    sex

--- logitpe23.py
+++ logitpe23.py
@@ -1,11 +1,13 @@
-from time import sleep
-i = int(input("sisesta aeg"))
-if i == 0:
-    print("0")
-while i >0:
-    print(i)
-    sleep(1)
-    i -= 1
-    if i == 0:
-        print("0")
-        break
+loppseeria = int(input("Sisestage loppseeria arv: "))
+summa = 0
+i = 1
+
+while i <= loppseeria:
+        summa += i
+        i += 1
+i = loppseeria - 1
+while i > 0:
+        summa += i
+        i -= 1
+
+print(summa)
