2e0f592afad7498ab754584131356ca8d96de8f9
zsh <sanderlapimaa@gmail.com>
2024-03-06 09:49:12 +0200

    fun

--- logitpe23.py
+++ logitpe23.py
@@ -1,39 +1,11 @@
-from tkinter import *
-from math import *
-import time
-raam = Tk()
-raam.title("Kell")
-w = 500
-h = 500
-tahvel = Canvas(raam, width=w, height=h, bg="black")
-tahvel.create_oval(10,10,w-10,h-10,fill="white")
-tahvel.create_oval(w/2-5,h/2-5,w/2+5,h/2+5,fill="white")
-sek_id = tahvel.create_line(w/2,h/2,w/2,20,fill="red")
-min_id = tahvel.create_line(w/2,h/2,w/2,20,fill="blue")
-h_id = tahvel.create_line(w/2,h/2,w/2,100,fill="blue")
-def uuenda():
-        sekundid = time.localtime().tm_sec
-        r = min(w/2,h/2)-20
-        x = r*cos(pi/2-sekundid/60.0*2*pi)
-        y = -r*sin(pi/2-sekundid/60.0*2*pi)
-        tahvel.coords(sek_id, 0, 0, x, y)
-        tahvel.move(sek_id, w/2, h/2)
-        
-        minutid = time.localtime().tm_min
-        r = min(w/2,h/2)-20
-        x = r*cos(pi/2-minutid/60.0*2*pi)
-        y = -r*sin(pi/2-minutid/60.0*2*pi)
-        tahvel.coords(min_id, 0, 0, x, y)
-        tahvel.move(min_id, w/2, h/2)
-        
-        tunnid = time.localtime().tm_hour
-        r = min(w/2,h/2)-90
-        x = r*cos(pi/2-tunnid/60.0*2*pi)
-        y = -r*sin(pi/2-tunnid/60.0*2*pi)
-        tahvel.coords(h_id, 0, 0, x, y)
-        tahvel.move(h_id, w/2, h/2)
-        raam.after(1000, uuenda)
-
-uuenda()
-tahvel.pack()
-raam.mainloop()
+import linecache
+failinimi = input("sisestage faili nimi: ")
+fail = open(failinimi, encoding = "UTF-8")
+i = 0
+for rida in fail:
+    i+=1
+    print(str(+i)+".",rida)
+fail.close
+test = int(input("sisesta number: "))
+loe = linecache.getline(failinimi, test)
+print(loe)
--- woah.py
+++ woah.py
@@ -1,8 +1,4 @@
-investeering = float(input("Sisestage investeeritav summa (eurodes): "))
-aastad = int(input("Sisestage investeeringu kogupikkus (aastates): "))
-tootlus_protsentides = float(input("Sisestage oodatav aastatootlus (protsentides): "))
-
-tootlus_kordaja = 1 + tootlus_protsentides / 100
-lõppsumma = investeering * (tootlus_kordaja ** aastad)
-
-print(f"Teie investeeringu lõppsumma {aastad} aasta pärast on: {round(lõppsumma, 2)} eurot")
+failinimi = input("sisestage faili nimi: ")
+fail = open(failinimi, encoding = "UTF-8")
+sisu = fail.readlines()
+print(sisu[0])
