45c5d39f95044047e4f536e92ca2dc6996287b55
zsh <sanderlapimaa@gmail.com>
2024-03-12 15:47:16 +0200

    s

--- logitpe23.py
+++ logitpe23.py
@@ -1,5 +1,14 @@
 sispin = input("sisestage pin: ")
+raha = 3000000
+orjad = 39
 if sispin == "1234":
-    print("pank")
+    print("pang:",raha)
+    print("sul on",orjad,"orja")
+    print("palju raha soovid:")
+    rahav = int(input())
+    if rahav <= raha:
+        raha -= rahav
+        print("teil on",raha,"eurot")
+    
 else:
     print("vale pin")
---
+++ s.py
@@ -0,0 +1,11 @@
+kood1 = "1223"
+kood2 = "0454"
+print("Sisesta kood:")
+sisestus1 = input()
+print("Sisesta kood:")
+sisestus2 = input()
+ 
+if sisestus1 == kood1 or sisestus2 == kood2:
+    print("ye")
+else:
+    print("nah")
